0c8fa71b9841a8684cbd364d68e8481ef5a75d8c
DeZoST <zosted62400@gmail.com>
2026-01-19 15:30:01 +0100

    Norminette

--- get_next_line.c
+++ get_next_line.c
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alede-ba <alede-ba@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alexis <alexis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 20:55:20 by alede-ba          #+#    #+#             */
-/*   Updated: 2026/01/18 21:15:06 by alede-ba         ###   ########.fr       */
+/*   Updated: 2026/01/19 15:28:30 by alexis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 /*
-Reads from fd and appends data to stash until a new line is found, EOF is reached, an error occurs
+Reads from fd and appends data to stash until a new line is found,
+EOF is reached or an error occurs
 */
 static char	*read_to_stash(int fd, char *stash)
 {
@@ -30,10 +31,12 @@ static char	*read_to_stash(int fd, char *stash)
 			return (NULL);
 		}
 		buffer[bytes_read] = '\0';
-		stash = ft_strjoin(stash, buffer);
+		if (bytes_read > 0)
+			stash = ft_strjoin(stash, buffer);
 		if (!stash)
 			return (NULL);
 	}
+	return (stash);
 }
 
 /*
@@ -56,7 +59,7 @@ static char	*extract_line(char *stash)
 	if (!line)
 		return (NULL);
 	i = 0;
-	while(stash[i] && stash[i] != '\n')
+	while (stash[i] && stash[i] != '\n')
 	{
 		line[i] = stash[i];
 		i++;
@@ -66,3 +69,126 @@ static char	*extract_line(char *stash)
 	line[i] = '\0';
 	return (line);
 }
+
+/*
+Removes the extracted line from stash and returns the remaining data
+*/
+static char	*save_remainder(char *stash)
+{
+	size_t	i;
+	size_t	j;
+	char	*new_stash;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+	{
+		free(stash);
+		return (NULL);
+	}
+	i++;
+	new_stash = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
+	if (!new_stash)
+	{
+		free(stash);
+		return (NULL);
+	}
+	j = 0;
+	while (stash[i])
+		new_stash[j++] = stash[i++];
+	new_stash[j] = '\0';
+	free(stash);
+	return (new_stash);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = read_to_stash(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = extract_line(stash);
+	if (!line)
+	{
+		free(stash);
+		stash = NULL;
+		return (NULL);
+	}
+	stash = save_remainder(stash);
+	return (line);
+}
+
+/*
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static void	print_title(char *title)
+{
+	printf("\n==============================\n");
+	printf("%s\n", title);
+	printf("==============================\n");
+}
+
+static void	run_test(int fd)
+{
+	char	*line;
+	while ((line = get_next_line(fd)))
+	{
+		write(1, line, ft_strlen(line));
+
+		free(line);
+	}
+	printf("Returned NULL\n");
+}
+
+int	main(void)
+{
+	int	fd;
+
+	print_title("Multiple lines");
+	fd = open("test.txt", O_RDONLY);
+	if (fd >= 0)
+	{
+		run_test(fd);
+		close(fd);
+	}
+
+	print_title("Single line without newline");
+	fd = open("single_no_nl.txt", O_RDONLY);
+	if (fd >= 0)
+	{
+		run_test(fd);
+		close(fd);
+	}
+
+	print_title("Empty file");
+	fd = open("empty.txt", O_RDONLY);
+	if (fd >= 0)
+	{
+		run_test(fd);
+		close(fd);
+	}
+
+	print_title("Only newlines");
+	fd = open("only_newlines.txt", O_RDONLY);
+	if (fd >= 0)
+	{
+		run_test(fd);
+		close(fd);
+	}
+
+	print_title("Invalid FD");
+	if (!get_next_line(-1))
+		printf("Correctly returned NULL\n");
+
+
+	return (0);
+}
+*/
